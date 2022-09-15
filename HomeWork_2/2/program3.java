ae9926abcc1f96fff8b65408d0d2f617ac183973
Maksim_Mikosha <mikmaksim@gmail.com>
2022-09-15 23:07:15 +0300

    Add draft HomeWork_2\2

---
+++ HomeWork_2/2/program3.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class program3 {
+    public static void main(String[] args) {
+        Logger log = Logger.getAnonymousLogger();
+        try {
+            File f  = new File("F:/GeekBrains/Java/JavaHomeWork/HomeWork_2/2/Rating.txt");
+            FileInputStream in = new FileInputStream(f);
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"))) {
+                String s;
+                while ((s = br.readLine()) != null) {
+                    String[] words = s.split(" ");
+                    log.log(Level.INFO, "Output string");
+                    System.out.printf("Ученик %s получил сегодня %s \n", words[0].toString(), words[1]);
+                }
+            } catch (Exception ex) {
+                log.log(Level.WARNING, "Something went wrong ", ex.getMessage());
+            }
+        } catch (Exception exp) {
+            log.log(Level.WARNING, "Something went wrong ", exp.getMessage());
+        }
+    }
+}
