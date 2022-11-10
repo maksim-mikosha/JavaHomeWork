e71343685f4e672524098911ff23f135c966561e
Maksim_Mikosha <mikmaksim@gmail.com>
2022-11-10 19:00:55 +0300

    Finish Exception\HomeWork_2\3

--- Exception/HomeWork_2/3/program/src/Main.java
+++ Exception/HomeWork_2/3/program/src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 90;
+        int b = 3;
+        try {
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = {1, 2};
+            abc[3] = 9;
+        } catch (ArithmeticException ex) {
+            System.out.println("Делить на ноль нельзя!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
+        }
+    }
+
+    public static void printSum(int a, int b) {
+        System.out.println(a + b);
     }
 }
